f654c68e6ec41c7478660e9451e9d984cb567c83
heinermad <heinermad88hotmail.com>
2021-10-09 23:03:36 -0500

    Tercer commit-nuevas vistas integradas

--- rutas.py
+++ rutas.py
@@ -2,4 +2,4 @@ from flask import Flask, app, render_template
 app = Flask(__name__)
 @app.route("/")
 def hola():
-    return render_template("crearEmpleado.html")
+    return render_template("paginaEmpleado.html")

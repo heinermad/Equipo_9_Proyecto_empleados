00d1aebcffdddbb9a042e36a559d43c82651afb9
wilmermad <wilmermad@hotmail.com>
2021-10-11 19:47:29 -0500

    Rutas CRUD y dashboard

--- rutas.py
+++ rutas.py
@@ -1,5 +1,27 @@
 from flask import Flask, app, render_template
 app = Flask(__name__)
-@app.route("/")
-def hola():
+
+
+@app.route("/", methods=["GET", "POST"])
+def inicio():
+    return render_template("dashboard.html")
+
+@app.route("/crear", methods=["GET", "POST"])
+def agregar():
+    return render_template("CrearEmpleado.html")
+
+@app.route("/buscar", methods=["GET", "POST"])
+def encontrar():
     return render_template("buscarEmpleados.html")
+
+@app.route("/editar", methods=["GET", "POST"])
+def actualizar():
+    return render_template("editarEmpleado.html")
+
+@app.route("/eliminar", methods=["GET", "POST"])
+def borrar():
+    return render_template("eliminarEmpleado.html")
+
+@app.route("/dashboard", methods=["GET", "POST"])
+def administrar():
+    return render_template("eliminarEmpleado.html")
